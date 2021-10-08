13649783959869b789b940f0a86f8ad89185ac05
HerotheHerotheGodofDeath <79506131+HerotheHerotheGodofDeath@users.noreply.github.com>
2021-10-08 14:20:09 +0200

    La till en clone powerup.

---
+++ Juicinezz/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }//Gör så att projectile sjuter rakt upp. Mattias.
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       /* Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);*/
+    }
+}
