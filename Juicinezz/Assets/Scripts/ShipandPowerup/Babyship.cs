784916e1339ea185885f2a0edfe58c0e97a3cc96
HerotheHerotheGodofDeath <79506131+HerotheHerotheGodofDeath@users.noreply.github.com>
2021-10-26 11:22:12 +0200

    Ändrat små saker + startat processen av att få den starka fienden att svära när den blivit skjuten.

--- Juicinezz/Assets/Scripts/ShipandPowerup/Babyship.cs
+++ Juicinezz/Assets/Scripts/ShipandPowerup/Babyship.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Babyship : Ship
 {
     public float CountDown = 15;
-    float timer;
+    float Deathtimer;
 
 
     private void Start()
@@ -13,8 +13,8 @@ public class Babyship : Ship
     public override void Update()
     {
         base.Update(); //tar Update() från Skep koden. Mattias.
-        timer += Time.deltaTime;
-        if (timer >= countDown)
+        Deathtimer += Time.deltaTime;
+        if (Deathtimer >= countDown)
         {
             Destroy(gameObject); //Dödar sig själv när countDown slår 0. Mattias.
         }
